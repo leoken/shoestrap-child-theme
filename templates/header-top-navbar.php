9ddfa4b49b5ef52a59fa9f16c9de68e6f9386482
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-03 18:41:31 +0200

    adding templates

---
+++ templates/header-top-navbar.php
@@ -0,0 +1,24 @@
+<header id="banner" class="navbar navbar-fixed-top" role="banner">
+  <div class="navbar-inner">
+    <div class="container">
+      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </a>
+      <a class="brand" href="<?php echo home_url(); ?>/">
+        <?php navbar_brand(); ?>
+      </a>
+      <nav id="nav-main" class="nav-collapse" role="navigation">
+        <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
+          endif;
+        ?>
+      </nav>
+      <ul id="nav-main" class="nav nav-collapse pull-right">
+        <?php do_action('shoestrap_nav_top_right'); ?>
+      </ul>
+    </div>
+  </div>
+</header>
