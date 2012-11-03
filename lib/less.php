0fb4c4a7bd11ab44e02b1442de7c21c2fcf48466
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-03 19:22:41 +0200

    responsive / fixed-width modes

--- lib/less.php
+++ lib/less.php
@@ -2,15 +2,8 @@
 
 function shoestrap_phpless(){
   
-  $shoestrap_responsive = get_theme_mod( 'shoestrap_responsive' );
-  
   $less = new lessc;
   // $less->setFormatter( "compressed" );
-  
-  if ( $shoestrap_responsive == '0' ) {
-    $less->checkedCompile( STYLESHEETPATH . '/assets/css/style-fixed.less', STYLESHEETPATH . '/assets/css/style-fixed.css' );
-  } else {
-    $less->checkedCompile( STYLESHEETPATH . '/assets/css/style-responsive.less', STYLESHEETPATH . '/assets/css/style-responsive.css' );
-  }
+  $less->checkedCompile( STYLESHEETPATH . '/assets/css/style.less', STYLESHEETPATH . '/assets/css/style.css' );
 }
 add_action('wp', 'shoestrap_phpless');
