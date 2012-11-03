5ebdaaa1ad3695907f0280c8bc79171643911644
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-03 19:08:59 +0200

    better stylesheets

--- lib/less.php
+++ lib/less.php
@@ -5,12 +5,12 @@ function shoestrap_phpless(){
   $shoestrap_responsive = get_theme_mod( 'shoestrap_responsive' );
   
   $less = new lessc;
-  $less->setFormatter( "compressed" );
+  // $less->setFormatter( "compressed" );
   
   if ( $shoestrap_responsive == '0' ) {
-    $less->checkedCompile( STYLESHEETPATH . '/assets/css/app-fixed.less', STYLESHEETPATH . '/assets/css/app-fixed.css' );
+    $less->checkedCompile( STYLESHEETPATH . '/assets/css/style-fixed.less', STYLESHEETPATH . '/assets/css/style-fixed.css' );
   } else {
-    $less->checkedCompile( STYLESHEETPATH . '/assets/css/app-responsive.less', STYLESHEETPATH . '/assets/css/app-responsive.css' );
+    $less->checkedCompile( STYLESHEETPATH . '/assets/css/style-responsive.less', STYLESHEETPATH . '/assets/css/style-responsive.css' );
   }
 }
 add_action('wp', 'shoestrap_phpless');
