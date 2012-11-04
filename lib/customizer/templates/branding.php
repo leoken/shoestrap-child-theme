714377645629836572701f15fa8ff429fb266ff3
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-04 06:30:57 +0200

    buxfix (social links on the customizer were present both on the navbar and header)

--- lib/customizer/templates/branding.php
+++ lib/customizer/templates/branding.php
@@ -7,7 +7,7 @@ function shoestrap_branding() {
         <div class="row-fluid">
           <?php require_once dirname( __FILE__ ) . '/logo.php'; ?>
           <ul class="pull-right social-networks">
-            <?php shoestrap_add_social_links(); ?>
+            <?php if ( has_action( 'shoestrap_branding_branding_right' ) ) { do_action( 'shoestrap_branding_branding_right' ); } ?>
           </ul>
         </div>
       </div>
