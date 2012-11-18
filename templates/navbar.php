a4ae00ef236f356324b4db4e629d751c897c6828
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-18 17:47:10 +0200

    correct navbar template

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,9 +1,27 @@
-<div class="container">
-  <div class="subnav">
-    <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav nav-pills'));
-      endif;
-    ?>
+<div id="main-subnav" class="container">
+  <div class="navbar" role="banner">
+    <div class="navbar-inner">
+      <div class="container">
+        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </a>
+        <?php if ( get_theme_mod( 'shoestrap_navbar_branding' ) != 0 ) { ?>
+          <a class="brand" href="<?php echo home_url(); ?>/">
+            <?php shoestrap_navbar_brand(); ?>
+          </a>
+        <?php } ?>
+        <?php do_action('shoestrap_nav_top_left'); ?>
+        <nav id="nav-main" class="nav-collapse" role="navigation">
+          <?php
+            if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
+            endif;
+          ?>
+        </nav>
+        <?php do_action('shoestrap_nav_top_right'); ?>
+      </div>
+    </div>
   </div>
 </div>
