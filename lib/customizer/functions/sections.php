ef07797327e3a260cb572a9b20c226bec4818e8b
Aristeides Stathopoulos <aristath@gmail.com>
2012-11-18 15:57:28 +0200

    Adding Navbar customizer section

--- lib/customizer/functions/sections.php
+++ lib/customizer/functions/sections.php
@@ -7,9 +7,10 @@ function shoestrap_register_sections( $wp_customize ){
   
   // remove default sections
   $sections   = array();
-  $sections[] = array( 'slug' => 'shoestrap_header',      'title' => __( 'Header & Logo', 'shoestrap' ),    'priority' => 1 );
-  $sections[] = array( 'slug' => 'shoestrap_layout',      'title' => __( 'Layout', 'shoestrap' ),           'priority' => 2 );
-  $sections[] = array( 'slug' => 'shoestrap_typography',  'title' => __( 'Typography', 'shoestrap' ),       'priority' => 3 );
+  $sections[] = array( 'slug' => 'shoestrap_navbar',      'title' => __( 'Navbar', 'shoestrap' ),           'priority' => 1 );
+  $sections[] = array( 'slug' => 'shoestrap_header',      'title' => __( 'Header & Logo', 'shoestrap' ),    'priority' => 2 );
+  $sections[] = array( 'slug' => 'shoestrap_layout',      'title' => __( 'Layout', 'shoestrap' ),           'priority' => 3 );
+  $sections[] = array( 'slug' => 'shoestrap_typography',  'title' => __( 'Typography', 'shoestrap' ),       'priority' => 4 );
   $sections[] = array( 'slug' => 'shoestrap_footer',      'title' => __( 'Footer', 'shoestrap' ),           'priority' => 5 );
   $sections[] = array( 'slug' => 'shoestrap_hero',        'title' => __( 'Hero', 'shoestrap' ),             'priority' => 7 );
   $sections[] = array( 'slug' => 'shoestrap_social',      'title' => __( 'Social Links', 'shoestrap' ),     'priority' => 8 );
